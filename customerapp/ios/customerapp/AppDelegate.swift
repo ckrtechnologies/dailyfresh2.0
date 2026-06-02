d08adc8b1b942e0f89661f7aca0eaa070b46f6c5
mallikera-spec <chandanmallik@gmail.com>
2026-06-03 00:24:38 +0530

    chore: update Xcode project build script configuration and paths

--- customerapp/ios/customerapp/AppDelegate.swift
+++ customerapp/ios/customerapp/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "customerapp",
+      withModuleName: "dailyfreshkolkata",
       in: window,
       launchOptions: launchOptions
     )
